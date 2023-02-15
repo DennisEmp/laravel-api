2ce442849fc6bbfe8d4e67f4d3aa7b763071dbf8
Dennis Empirio <dennis.empirio@gmail.com>
2023-02-15 17:47:13 +0100

    Home css DONE

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,30 +1,32 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1 class="text-danger">Movie Categories</h1>
+    <h1 class="text-center bg-dark text-danger">Movie Categories</h1>
 
     @foreach ($genres as $genre)
 
-    <div class="card text-center my-5">
+    <div class="card my-5">
 
-        <div class="card-header">
+        <div class="card-header bg-dark text-danger text-center">
             <h3><strong>Genre:</strong> {{ $genre -> name }}</h3>
         </div>
 
         {{-- container contenente tutti i film che appartengono al genere --}}
-        <div class="card-body d-flex flex-wrap gap-3">
+        <div class="card-body">
 
-            {{-- card del film --}}
-            @foreach ($genre -> movies as $movie)
-                <div class="card" style="width: 18rem;">
-                    <div class="card-body text-start">
-                        <h5 class="card-title">{{ $movie -> name }}</h5>
-                        <span>Release Date: {{ $movie -> release_date }}</span>
-                        <br>
-                        <span>Cash Out: {{ $movie -> cashOut }}</span>
-                    </div>
-                </div>   
-            @endforeach
+            {{-- griglia di card del film --}}
+            <div class="d-flex justify-content-center flex-wrap">
+                @foreach ($genre -> movies as $movie)
+                    <div class="card m-3" style="width: 18rem;">
+                        <div class="card-body text-start">
+                            <h5 class="card-title">{{ $movie -> name }}</h5>
+                            <span>Release Date: {{ $movie -> release_date }}</span>
+                            <br>
+                            <span>Cash Out: {{ $movie -> cashOut }}</span>
+                        </div>
+                    </div>   
+                @endforeach
+            </div>
 
         </div>
 
