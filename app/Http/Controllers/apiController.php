9b557fa2a224073849b558ae53a6e91be9a49e46
Dennis Empirio <dennis.empirio@gmail.com>
2023-02-20 17:47:47 +0100

    api cntrll aggiornato

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Movie;
+use App\Models\Genre;
+use App\Models\Tag;
+
 class apiController extends Controller
 {
-    //
+    public function getMovieWTagWGenre() {
+
+        $movies = Movie :: with('tags') 
+            -> orderBy('created_at', 'desc')
+            -> get();
+        $genres = Genre :: all();
+        $tags = Tag :: all();
+
+        return response() -> json([
+            'success' => true,
+            'response' => [
+                'movies' => $movies,
+                'genres' => $genres,
+                'tags' => $tags,
+            ]
+        ]);
+    }
 }
